942b9440a3b66bca8664efe48cfa6300e852b027
turanlz <turanhasan013@gmail.com>
2026-02-08 21:00:48 +0400

    Refactor homepage layout: removed old static content and replaced it with modular components including Header, HeroSection, BlogSection, and ContactSection for improved structure and maintainability.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,27 +9,6 @@ import { Footer } from "@/components/layout/footer/footer";
 
 export default function Home() {
   return (
-<<<<<<< HEAD
-    <main className="min-h-dvh bg-[#0c1018] flex flex-col items-center justify-center px-4 text-center">
-      <div className="relative w-40 h-40 mb-6">
-        <Image 
-          src="/images/global/logo.png" 
-          alt="Zest Construction Logo"
-          fill
-          className="object-contain"
-          priority
-        />
-      </div>
-      <p className=" tracking-tight mt-4 text-lg md:text-xl text-[#BFBFBF] font-light italic">
-        welcome to zest development!
-      </p>
-      <h1 className="text-3xl md:text-3xl font-bold text-[#F8F8F8]">
-        This website is under development!
-      </h1>
-      <h1 className="mt-3 text-3xl md:text-3xl font-bold text-[#F8F8F8] tracking-tight">
-        COMING SOON...
-      </h1>
-=======
     <main className="relative">
       <Header />
       <HeroSection />
@@ -39,7 +18,6 @@ export default function Home() {
       <BlogSection />
       <ContactSection />
       <Footer />
->>>>>>> development
     </main>
   );
 }
